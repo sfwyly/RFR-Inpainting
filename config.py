a4b8ce2328eeccc962b475d8489e7cb835bd821f
sfwyly <31818508+sfwyly@users.noreply.github.com>
2020-11-30 12:34:34 +0800

    Configuration

---
+++ config.py
@@ -0,0 +1,29 @@
+
+
+#batch
+batch_size = 6
+batch = 20   # batch_size * batch
+#image size
+image_size = 256
+
+image_channel = 3
+mask_channel = 3
+
+#learning_rate
+learning_rate = 1e-4
+decay = 0.02
+
+# loss parameter
+p1 = 120
+p2 = 0.05
+p3 = 1
+p4 = 5
+p5 = 0.01
+
+# epochs
+epochs  = 100
+
+iteration = 20
+
+#save frequency
+save_bacth = 10
